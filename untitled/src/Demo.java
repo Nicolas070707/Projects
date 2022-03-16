e3ffb0ffe43b5fc2dc26f48d75436cd3ba634d2f
nicolas <nicolas.theiner@student.htldornbirn.at>
2022-03-16 15:48:10 +0100

    added main files

---
+++ untitled/src/Demo.java
@@ -0,0 +1,24 @@
+import java.sql.SQLOutput;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Moin");
+
+        int c=10;
+        double d =10.7;
+
+        boolean isHuman =true;
+        String text = "Ich heiße Fickidich";
+
+        if (c==4){
+            System.out.println("its 4");
+        } else {
+            System.out.println("else");
+        }
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("i:" +i);
+            
+        }
+    }
+}
